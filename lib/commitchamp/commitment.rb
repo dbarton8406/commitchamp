2c4560d6fcd1faae659b30f5c89f38b8e594d011
Daniel <dbarton8406@gmail.com>
2015-10-18 10:42:24 -0400

    initial module commit

---
+++ lib/commitchamp/commitment.rb
@@ -0,0 +1,31 @@
+module Commitchamp
+  class Contributions
+    
+    include HTTParty
+    base_uri "https://api.github.com"
+
+
+    def initialize(auth_token)
+      @auth = {
+          "Authorization" =>  "token #{auth_token}",
+          "User-Agent"    =>  "HTTParty"
+      }
+    end
+
+    def get_org_repo(org, options={})
+    	 self.class.get("/orgs/#{org}/repos",
+                    :headers => @header,
+                     body: options.to_jason)
+    end
+
+    def get_contributors(owner, repo)
+        self.class.get("/repos/#{owner}/#{repo}/contributors",
+                     :headers => @auth
+    end	
+    
+
+    def get_contributions_stats(owner, repo)
+    	self.class.get("/repos/#{owner}/#{repo}/stats/contributors",
+    		          :headers => @auth)
+    end
+
